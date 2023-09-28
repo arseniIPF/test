8091529ccb8dd05e645a17efcf3b0189260b7e58
Arseni Hernández <114494544+arseniIPF@users.noreply.github.com>
2023-09-28 08:43:17 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,59 @@
+tiles.loadMap(tiles.createMap(tilemap`nivel1`))
+let homonet = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+let cosa = sprites.create(img`
+    ............3333bb..bb33333.....
+    ........3bb31111d3b311d111d33...
+    .......3bdd11111dbd11d11111113..
+    .......bdddd1111bd11d111dd11113.
+    ......3d111dd111b11d111dd33d11d3
+    ......3d11111dd1d11d111d11d33113
+    ....bb3d111111dd13dd111d1dd3b31b
+    ...b3d3dd111111dd13dd11d1dddbbdb
+    ...3ddd31d111111dd133dddddddb.b.
+    ..311111d1ddd1111dd11dddddd33...
+    ..3111111d111dd111dd1111dd3313..
+    ..bddd1111ddd11dd111d111111113..
+    ..311ddd111dddd11dd11ddd1111ddb.
+    ..31111dd111dddd11dd111dddddddb.
+    ...bd1111d1113ddd11dd1111111d3b.
+    ...4dd1111d1113ddd11ddd111d333b.
+    ..4dbdddd11d11133ddddddddddddb..
+    .4ddbddddd11d111d33ddddddddd3b..
+    .4dddb11ddd11dd111d333dddd3bb...
+    .4dd55b111d11dd11111d3333bbb....
+    .445555b111d11dddd111111ddb.....
+    .4455555bd1d311ddddddddddd3.....
+    .45455555bb1d3111ddddddd113.....
+    .4554555555b333d1111111113......
+    455554555555bbb33d11111d33......
+    4d555545555555dbbb3d11d33.......
+    4dd5555455555ddddd43333.........
+    45dd555544ddddddd4..............
+    .45dd5555d44dddd4...............
+    ..45dd55dddd4444................
+    ...45dd55444....................
+    ....44444.......................
+    `, SpriteKind.Food)
+cosa.setBounceOnWall(true)
+controller.moveSprite(homonet)
+cosa.setVelocity(50, 50)
+if (homonet.overlapsWith(cosa)) {
+    music.play(music.createSoundEffect(WaveShape.Triangle, 300, 200, 255, 0, 75, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+}
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Código generado automáticamente. No editar.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "nivel1":
+            case "nivel1":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010201010101010101010101010101010101010101010101010104040404040405010101010101010101010101010101060405010101020101010101010201010101060707070707070701010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010201010101010101010101010101010101010101010101010101010101010101010101010101010101010102010101010101010101010101010101010101010101010102010101010101010101010201010101010101010103030303030303030303030303030303`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.builtin.forestTiles0,sprites.swamp.swampTile2,sprites.swamp.swampTile7,sprites.swamp.swampTile8,sprites.swamp.swampTile12,sprites.swamp.swampTile9], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
